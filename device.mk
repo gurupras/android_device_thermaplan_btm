fa2a84145ca23848a01836d4e79835af3b87d6ac
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2018-08-20 16:38:38 -0400

    Minor fixes

--- device.mk
+++ device.mk
@@ -5,19 +5,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
     sys.thermaplan.enabled=1
 
 BOARD_SEPOLICY_DIRS += \
-    $(LOCAL_PATH)/sepolicy
+    device/thermaplan-btm/sepolicy
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init.thermaplan.rc:root/init.thermaplan.rc \
-    $(LOCAL_PATH)/su_daemon:system/bin/su_daemon
+    device/thermaplan-btm/init.thermaplan.rc:root/init.thermaplan.rc \
+    device/thermaplan-btm/su_daemon:system/bin/su_daemon
 
 ifeq ($(THERMAPLAN_DISABLE_CPR_REGULATOR),true)
     $(warning "Including initrc file to disable CPR regulator")
     PRODUCT_PROPERTY_OVERRIDES += \
         sys.thermaplan.cpr=1
 
-    PRODUCT_COPY_FILES += \
-        $(LOCAL_PATH)/init.thermaplan_cpr.rc:root/init.thermaplan_cpr.rc
+#    PRODUCT_COPY_FILES += \
+#        device/thermaplan-btm/init.thermaplan_cpr.rc:root/init.thermaplan_cpr.rc
 endif
 
 endif
